fb0569184660f3dd8fc96fb5d26f7e51d1f89929
Ashnil <85815731+ashnike@users.noreply.github.com>
2024-05-09 08:38:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "jenkastore"
     key            = "folder/statefile.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "Jenkaftable"
     encrypt        = true
   }
